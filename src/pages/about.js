6840ead3e836a3e7525d82b384e41952dcd2bb01
Jack Painter <littlesympathy@jacks-mbp.home>
2020-05-07 11:59:13 +0100

    Add filler photo and text to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,66 @@
 import React from "react"
+import styled from "styled-components"
 
 // Components
 import Layout from "../components/layout"
 
+// Images 
+import Face from "../images/myFace.png";
+
+const StyledP = styled.p`
+    font-size: 1.2rem;
+    max-width: 700px;
+`
+
+const StyledImg = styled.img`
+  width: 130%;
+  height: 100%;
+`
+
+const StyledImgContainer = styled.div`
+  flex: 1;
+  width: 200px;
+  height: 200px;
+  box-sizing: border-box;
+  border: 2px black solid;
+  background: white;
+  border-radius: 10%;
+  overflow: hidden;
+  box-shadow: 0 5px 15px 0px rgba(0, 0, 0, 0.6);
+  transform: translatey(0px);
+  animation: float 6s ease-in-out infinite;
+`
+
 const About = () => {
-    return (
-        <Layout>
-            
-        </Layout>
-    )
+  return (
+    <Layout>
+    <div className="About-Page" id="About">
+    <div className="About-Container">
+      <StyledImgContainer className="Float-Me-About">
+        <StyledImg className="Face" src={Face} alt="My Face"/>
+      </StyledImgContainer>
+      <h1 className="About-header">About Me</h1>
+      <StyledP>
+        Hey I'm Jack, an enthusiastic and passionate junior
+        full-stack developer. After more than 4 years of working on the sales
+        side of IT, I decided enough was enough and made the decision to drop it
+        all and become a developer.
+        <br />
+        <br />
+        I have just completed a 2-month  agile full-stack development course with Kodiri
+        where I  predominately worked with JavaScript, React, Node, HTML and CSS. I made 
+        a Netflix clone, bill calulator and a messaging application whilst there and I am 
+        now planning to create a Tetris application using hook based functional components.
+        <br />
+        <br />
+        When I'm not coding you'll find me playing about with my DSLR (any tips would be very
+        welcome), gaming, listening to vinyls, attending meetups and following
+        the Formula 1 & football.
+      </StyledP>
+    </div>
+    </div>
+    </Layout>
+  )
 }
 
 export default About
