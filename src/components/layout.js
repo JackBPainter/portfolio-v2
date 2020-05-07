16176362da15db22c64af1c1f90775e8a5d6b2cf
Jack Painter <littlesympathy@jacks-mbp.home>
2020-05-07 11:23:05 +0100

    Create basic About component

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,6 +6,7 @@
  */
 
 import React from "react"
+import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header/header"
@@ -39,8 +40,8 @@ const Layout = ({ children }) => {
   )
 }
 
-// Layout.propTypes = {
-//   children: PropTypes.node.isRequired,
-// }
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
 
 export default Layout
