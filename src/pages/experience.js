d4332e89e7bb8739cf639bd023b241e0f4ba8dee
Jack Painter <littlesympathy@jacks-mbp.home>
2020-05-11 13:24:09 +0100

    Create experience query

--- src/pages/experience.js
+++ src/pages/experience.js
@@ -1,8 +1,32 @@
 import React from "react"
+import { graphql } from "gatsby"
+
+// Components
 import Layout from "../components/layout"
 
-const Experience = () => {
-    return <Layout />
+const Experience = ({ data }) => {
+    console.log(data)
+
+    return (
+        <Layout>
+
+        </Layout>
+    )
 }
 
 export default Experience 
+
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            title
+          }
+          rawMarkdownBody
+        }
+      }
+    }
+  }
+`
