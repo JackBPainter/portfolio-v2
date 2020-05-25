d36d34e4c63bd0cd4e815fb7ed1405c28debabbc
Jack Painter <littlesympathy@jacks-mbp.home>
2020-05-25 12:46:55 +0100

    Create contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useState } from "react"
 import Layout from "../components/layout"
 
 const Contact = () => {
-    return (
-        <Layout>
-        </Layout>
-    )
+  // An input form whose value is controlled by React is known as a controlled component
+  const [firstName, setFirstName] = useState("")
+  const [surname, setSurname] = useState("")
+  const [email, setEmail] = useState("")
+  const [number, setNumber] = useState(0)
+
+  return (
+    <Layout>
+      <form name="contact">
+        <label>
+          First Name
+          <input type="text" name="firstName" />
+        </label>
+        <label>
+          Last Name
+          <input type="text" name="lastName" />
+        </label>
+        <label>
+          Email
+          <input type="email" name="email" />
+        </label>
+        <label>
+          Number
+          <input type="number" name="number" />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </Layout>
+  )
 }
 
 export default Contact
