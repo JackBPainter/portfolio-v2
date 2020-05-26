f5c10ce5b8d270799c3c959372acadec1533eb5e
Jack Painter <littlesympathy@jacks-mbp.home>
2020-05-26 16:51:51 +0100

    Add projects.js page back

---
+++ src/pages/projects.js
@@ -0,0 +1,63 @@
+import React from "react"
+import styled from "styled-components"
+
+// Components
+import Layout from "../components/Layout"
+import ProjectBox from "../components/ProjectBox"
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 20px;
+`
+
+const StyledH1 = styled.h1`
+  font-size: 35px;
+  font-weight: 600;
+  max-width: 680px;
+`
+
+const StyledSpan = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  max-width: 750px;
+`
+
+const Projects = ({ data }) => {
+  const tetris = data.allMarkdownRemark.edges[7].node
+  const programateur = data.allMarkdownRemark.edges[6].node
+  const originalPortfolio = data.allMarkdownRemark.edges[5].node
+  const currentPortfolio = data.allMarkdownRemark.edges[2].node
+
+  return (
+    <Layout>
+      <StyledDiv>
+        <StyledH1>Projects</StyledH1>
+        <StyledSpan>
+          <ProjectBox
+            title={tetris.frontmatter.title}
+            skills={tetris.frontmatter.skills}
+          />
+          <ProjectBox
+            title={programateur.frontmatter.title}
+            skills={programateur.frontmatter.skills}
+          />
+          <ProjectBox
+            title={originalPortfolio.frontmatter.title}
+            skills={originalPortfolio.frontmatter.skills}
+          />
+          <ProjectBox 
+            title={currentPortfolio.frontmatter.title}
+            skills={currentPortfolio.frontmatter.skills}
+          />
+        </StyledSpan>
+      </StyledDiv>
+    </Layout>
+  )
+}
+
+export default Projects
