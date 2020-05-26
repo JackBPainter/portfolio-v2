20124a703b45839735f78a2b2e305f6e72664804
Jack Painter <littlesympathy@jacks-mbp.home>
2020-05-26 12:36:15 +0100

    Complete styling on contact inputs and textarea

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,11 +7,28 @@ import Layout from "../components/Layout"
 const StyledForm = styled.form`
   display: flex;
   flex-direction: column;
+  width: 300px;
 `
 
 const StyledInput = styled.input`
   border-radius: 2%;
   border: none;
+  font-family: sans-serif;
+  box-shadow: 0 5px 15px 0px rgba(0,0,0,0.6);
+  margin-bottom: 20px;
+  padding: 10px;
+`
+
+const StyledMessage = styled.textarea`
+  border-radius: 2%;
+  border: none;
+  font-weight: 400;
+  font-size: 13.3333px;
+  font-family: sans-serif;
+  box-shadow: 0 5px 15px 0px rgba(0,0,0,0.6);
+  margin-bottom: 10px;
+  padding: 10px;
+  height: 200px;
 `
 
 const Contact = () => {
@@ -23,10 +40,11 @@ const Contact = () => {
   return (
     <Layout>
       <StyledForm name="contact">
-          <StyledInput placeholder="First Name" type="text" name="firstName" />
-          <StyledInput placeholder="Last Name" type="text" name="lastName" />
+          <StyledInput placeholder="Name" type="text" name="name" />
           <StyledInput placeholder="Email" type="email" name="email" />
           <StyledInput placeholder="Number" type="number" name="number" />
+          <StyledInput placeholder="Subject" type="text" name="subject" />
+          <StyledMessage placeholder="Your message" type="text" name="message" />
         <button type="submit">Submit</button>
       </StyledForm>
     </Layout>
