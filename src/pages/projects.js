019a4b6122cf0278de773a7a763a8893c0dbc726
Jack Painter <littlesympathy@jacks-mbp.home>
2020-05-26 14:35:18 +0100

    Add arrow svg to contact page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -14,15 +14,9 @@ const StyledDiv = styled.div`
 `
 
 const StyledH1 = styled.h1`
-  display: flex;
-  justify-content: space-between;
   font-size: 35px;
   font-weight: 600;
   max-width: 680px;
-
-  @media (max-width: ${({ theme }) => theme.mobile}) {
-    flex-direction: column;
-  }
 `
 
 const StyledSpan = styled.div`
