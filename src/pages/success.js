3272e848fa4c25ef927c46a6cbbe4db5d9db4283
Jack Painter <littlesympathy@jacks-mbp.home>
2020-06-11 12:16:09 +0100

    Complete success page layout

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,26 +1,55 @@
 import React from "react"
 import styled from "styled-components"
+import { Link } from "gatsby"
 
 // Components
 import Layout from "../components/Layout"
 
+const StyledDiv = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-direction: column;
+  height: 300px;
+  width: 300px;
+  margin-top: 60px;
+  border-radius: 3%;
+  box-shadow: 0 5px 15px 0px rgba(0, 0, 0, 0.6);
+  background: ${({ theme }) => theme.primaryColor};
+
+  svg {
+      fill: ${({ theme }) => theme.secondaryColor}
+  }
+
+  a {
+    &:hover {
+      color: ${({ theme }) => theme.primaryHover};
+    }
+  }
+`
+
 const StyledH1 = styled.h1`
-  margin: 0 auto;
   font-size: 35px;
   font-weight: 600;
-  margin-top: 60px;
-  max-width: 480px;
+  max-width: 280px;
+  color: ${({ theme }) => theme.secondaryColor};
   transition: all 0.3s linear;
   text-align: center;
   align-items: center;
 `
 
 const Success = () => {
-    return (
-        <Layout>
-            <StyledH1>Thanks for reaching out, I will get back to you soon!</StyledH1>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <StyledDiv>
+        <StyledH1>
+          Thanks for reaching out, I will get back to you soon!
+        </StyledH1>
+        <svg height="55px" width="63px"><polygon points="32,-0.32 0.68,31 11,31 11,55 25,55 25,41 38,41 38,55 52,55 52,31 63.319,31 "/></svg>
+      </StyledDiv>
+    </Layout>
+  )
 }
 
 export default Success
+
