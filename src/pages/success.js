634404d62295629939f4862486a921a9fc5b6789
Jack Painter <littlesympathy@jacks-mbp.home>
2020-06-11 12:30:26 +0100

    Make home svg link to home and add hover color

--- src/pages/success.js
+++ src/pages/success.js
@@ -10,6 +10,7 @@ const StyledDiv = styled.div`
   justify-content: space-evenly;
   align-items: center;
   flex-direction: column;
+  transition: all 0.3s linear;
   height: 300px;
   width: 300px;
   margin-top: 60px;
@@ -18,12 +19,11 @@ const StyledDiv = styled.div`
   background: ${({ theme }) => theme.primaryColor};
 
   svg {
-      fill: ${({ theme }) => theme.secondaryColor}
-  }
+    fill: ${({ theme }) => theme.secondaryColor};
+    transition: all 0.3s linear;
 
-  a {
     &:hover {
-      color: ${({ theme }) => theme.primaryHover};
+      fill: ${({ theme }) => theme.primaryHover};
     }
   }
 `
@@ -45,11 +45,14 @@ const Success = () => {
         <StyledH1>
           Thanks for reaching out, I will get back to you soon!
         </StyledH1>
-        <svg height="55px" width="63px"><polygon points="32,-0.32 0.68,31 11,31 11,55 25,55 25,41 38,41 38,55 52,55 52,31 63.319,31 "/></svg>
+        <Link to="/">
+          <svg height="55px" width="63px">
+            <polygon points="32,-0.32 0.68,31 11,31 11,55 25,55 25,41 38,41 38,55 52,55 52,31 63.319,31 " />
+          </svg>
+        </Link>
       </StyledDiv>
     </Layout>
   )
 }
 
 export default Success
-
